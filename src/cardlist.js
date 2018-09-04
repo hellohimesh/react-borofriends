732482cfe7824e61ebc8b8867b0adb88e4517a06
himeshsuthar <himesh5983@gmail.com>
2018-09-05 04:49:28 +0530

    cards generated

---
+++ src/cardlist.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Card from './card';
+
+const CardList = ({robots}) => {
+const cardComp = robots.map(user=>{
+  return <Card id={user.id} name = {user.name} username = {user.username} email = {user.email} />
+})
+//
+// const cardComp = robots.map((user,i) =>{
+//   return <Card id={robots[i].id} name = {robots[i].name} username = {robots[i].username} email = {robots[i].email} />
+//
+// })
+return (
+  <div>
+    {cardComp}
+  </div>
+)
+}
+
+export default CardList;

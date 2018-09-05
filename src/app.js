0bcacdf3c916347493dee83ed820854070dae523
himeshsuthar <himesh5983@gmail.com>
2018-09-05 07:22:23 +0530

    server data fetched

--- src/app.js
+++ src/app.js
@@ -8,11 +8,19 @@ class App extends Component{
   constructor(){
     super();
     this.state = {
-      robots : robots,
+      robots : [],
       searchField : ''
     }
   }
+componentDidMount(){
+  fetch('https://jsonplaceholder.typicode.com/users').then(res => {
+    return res.json();
+  })
+  .then(users=>{
+  this.setState({robots: users})
+  })
 
+}
    onSearchChange = (event) => {
      this.setState({searchField:event.target.value});
   }
@@ -20,6 +28,10 @@ class App extends Component{
     const filterCopy = this.state.robots.filter(robot=>{
       return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase());
     })
+    if(this.state.robots.length == 0){
+      return <h1> Loading .. </h1>
+    }
+    else {
     return(
       <div className="tc">
         <h1>RoboFriends</h1>
@@ -27,7 +39,7 @@ class App extends Component{
         <CardList robots = {filterCopy} />
       </div>
     )
-
+  }
   }
 }
 

81761cb77d3e3576078c3eaae478af17081e39f8
himeshsuthar <himesh5983@gmail.com>
2018-09-05 06:34:09 +0530

    search implemented

--- src/app.js
+++ src/app.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React,{Component} from 'react';
 import {robots} from './robots';
 import CardList from './cardlist';
+import Searchbox from './searchbox';
 
-const App =()=>{
-return(
-  <CardList robots = {robots} />
-)
+
+class App extends Component{
+  constructor(){
+    super();
+    this.state = {
+      robots : robots,
+      searchField : ''
+    }
+  }
+
+   onSearchChange = (event) => {
+     this.setState({searchField:event.target.value});
+  }
+  render(){
+    const filterCopy = this.state.robots.filter(robot=>{
+      return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase());
+    })
+    return(
+      <div className="tc">
+        <h1>RoboFriends</h1>
+        <Searchbox searchChange = {this.onSearchChange} />
+        <CardList robots = {filterCopy} />
+      </div>
+    )
+
+  }
 }
 
 export default App;
